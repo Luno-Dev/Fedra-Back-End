86ab7c32d284e692e27ca3f5966388e5bb0e08b0
luciano rivera <lucianonicolasrivera8@gmail.com>
2023-06-08 12:42:14 -0300

    hablitamos cors

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dbConnection = require("../database/config")
+const cors = require("cors")
 class Server {
   constructor() {
     this.app = express();
@@ -20,6 +21,7 @@ class Server {
 
 
   middleware(){
+    this.app.use(cors())
     this.app.use(express.json())
     this.app.use(express.static("public"))
    
