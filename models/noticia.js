b2d9ca7d6034e1a19ff8f9a280b1817a344fa930
luciano rivera <lucianonicolasrivera8@gmail.com>
2023-06-08 10:43:23 -0300

    terminamos modelo de noticias

---
+++ models/noticia.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const NoticiaSchema = Schema({
+    titulo: {
+        type: String,
+        required: [true, "El nombre es obligatorio"],
+      },
+      descripcion:{
+        type: String,
+        required: [true, "La descripcion es obligatoria"],
+      },
+      autor:{
+        type: String,
+        required: [true, "El autor es obligatorio"],
+      },
+      fecha:{
+        type: String,
+        required: [true, "El autor es obligatorio"],
+      },
+      img: {
+        type: String,
+      },
+      estado: {
+        type: Boolean,
+        default: true,
+        required:true,
+      },
+      
+    
+})
+
+module.exports = model("Noticia", NoticiaSchema);
