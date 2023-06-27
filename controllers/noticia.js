254d30f116e733c67eb13a64b6d494a020dda8de
luciano rivera <lucianonicolasrivera8@gmail.com>
2023-06-27 13:08:30 -0300

    sacmos paginacion

--- controllers/noticia.js
+++ controllers/noticia.js
@@ -2,7 +2,7 @@ const Noticia = require("../models/noticia");
 
 // get para traer todos los productos paginados
 const obtenerNoticias = async (req, res) => {
-  const { limite = 5, desde = 0 } = req.query;
+  const { limite = 0, desde = 0 } = req.query;
   const query = { estado: true };
 
   const [total, noticias] = await Promise.all([
