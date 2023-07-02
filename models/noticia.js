5303167f41decf69e7f592206a1387bb22e6c4bf
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-07-02 17:25:47 -0300

    se mejora modelo noticias

--- models/noticia.js
+++ models/noticia.js
@@ -1,36 +1,36 @@
 const { Schema, model } = require("mongoose");
 
 const NoticiaSchema = Schema({
-    titulo: {
-        type: String,
-        required: [true, "El nombre es obligatorio"],
-      },
-      descripcion:{
-        type: String,
-        required: [true, "La descripcion es obligatoria"],
-      },
-      autor:{
-        type: String,
-        required: [true, "El autor es obligatorio"],
-      },
-      fecha: {
-        type: Date,
-        default: Date.now(),
-      },
-      img: [{
-        url:{
-          type: String,
-        }
-         ,
-      }
-      ],
-      estado: {
-        type: Boolean,
-        default: true,
-        required:true,
-      },
-      
-    
+  titulo: {
+    type: String,
+    required: [true, "El nombre es obligatorio"],
+  },
+  descripcion: {
+    type: String,
+    required: [true, "La descripcion es obligatoria"],
+  },
+  autor: {
+    type: String,
+    required: [true, "El autor es obligatorio"],
+  },
+  fecha: {
+    type: Date,
+    default: Date.now(),
+  },
+  img: [{
+
+    type: String,
+
+
+  }
+  ],
+  estado: {
+    type: Boolean,
+    default: true,
+    required: true,
+  },
+
+
 })
 
 module.exports = model("Noticia", NoticiaSchema);
