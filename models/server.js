620b9a484c71c76f625faf351cc1f4589c0caa21
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-07-18 00:00:46 -0300

    se agrega cosas a header

--- models/server.js
+++ models/server.js
@@ -25,6 +25,12 @@ class Server {
 
   middleware(){
     this.app.use(cors({origin:"*"}))
+    this.app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+      next();
+    })
     this.app.use(express.json())
     this.app.use(express.static("public"))
    
