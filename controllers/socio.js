23eadf9cd4ee75371b2fdaca9b19d82bab956557
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-08-03 21:09:30 -0300

    se realizan mejoras en post empleados

--- controllers/socio.js
+++ controllers/socio.js
@@ -5,33 +5,45 @@ const Empleados = require('../models/empleados');
 
 
 const sociosGet = async (req, res) => {
- 
-  const socios = await Socio.find().populate("empleados", "trabajadornombre") //{estado:true} Lo saque para que traiga todos los socios hasta los inactivos
+
+  const socios = await Socio.find().populate("empleados", "eId") //{estado:true} Lo saque para que traiga todos los socios hasta los inactivos
   const total = await Socio.countDocuments()
 
   res.json({
     total,
-   socios,
-    
+    socios,
+
   });
 };
 
+const obtenerSocio = async (req, res) => {
+  const { id } = req.params;
 
-/* const empleadosGet = async (req, res) => {
- 
-  const socios = await Empleados.find().populate("empleados","trabajadornombre") //{estado:true} Lo saque para que traiga todos los socios hasta los inactivos
-  const total = await Empleados.countDocuments()
+  const socio = await Socio.findById(id);
 
   res.json({
-    total,
-   socios,
+    socio,
+  });
+};
+
+const socioEmpleado = async (req, res) => {
+ 
+  const empleados = await Empleados.find({socio:"64b235b6d9c7a401705d7cb6"});//{estado:true} Lo saque para que traiga todos los socios hasta los inactivos
+
+  const { id } = req.params;
+
+  const empleador = await Socio.findById(id);
+
+  res.json({
+    empleador,
+   empleados
     
   });
-}; */
+}; 
 
 const sociosPost = async (req, res) => {
 
-  
+
   const {
     email,
     trabajadornombre,
@@ -62,130 +74,123 @@ const sociosPost = async (req, res) => {
     empleadortrabajolocalidad,
     empleadortrabajoprovincia,
     empleadoractividad,
-   password,
-   role,
+    password,
+    role,
     img,
- 
+
   } = req.body;
 
-  const socio = new Socio(req.body );
+  const socio = new Socio(req.body);
 
   const salt = bcrypt.genSaltSync();
   socio.password = bcrypt.hashSync(password, salt);
 
   await socio.save();
- res.status(201).json({
-    msg:"Socio creado con exito!",
+  res.status(201).json({
+    msg: "Socio creado con exito!",
     socio,
   });
 };
 
 
 const empleadosPost = async (req, res) => {
-const empleado = req.body;
+  const empleado = req.body;
 
-const {
-  email,
-  trabajadornombre,
-  trabajadorsueldo,
-  trabajadorapellido,
-  trabajadornacionalidad,
-  trabajadorestadocivil,
-  trabajadorsexo,
-  trabajadornacimiento,
-  trabajadordocumento,
-  trabajadorcuil,
-  trabajadordomicilio,
-  trabajadornumdomicilio,
-  trabajadorpiso,
-  trabajadordepto,
-  trabajadorlocalidad,
-  trabajadorprovincia,
-  trabajadorlugardetrabajo,
-  trabajadortareas,
-  trabajadortel,
-  trabajadorcel
+  const {
+    email,
+    trabajadornombre,
+    trabajadorsueldo,
+    trabajadorapellido,
+    trabajadornacionalidad,
+    trabajadorestadocivil,
+    trabajadorsexo,
+    trabajadornacimiento,
+    trabajadordocumento,
+    trabajadorcuil,
+    trabajadordomicilio,
+    trabajadornumdomicilio,
+    trabajadorpiso,
+    trabajadordepto,
+    trabajadorlocalidad,
+    trabajadorprovincia,
+    trabajadorlugardetrabajo,
+    trabajadortareas,
+    trabajadortel,
+    trabajadorcel
 
-} = req.body;
+  } = req.body;
 
-const data = {
-  trabajadornombre,
-  trabajadorsueldo,
-  trabajadorapellido,
-  trabajadornacionalidad,
-  trabajadorestadocivil,
-  trabajadorsexo,
-  trabajadornacimiento,
-  trabajadordocumento,
-  trabajadorcuil,
-  trabajadordomicilio,
-  trabajadornumdomicilio,
-  trabajadorpiso,
-  trabajadordepto,
-  trabajadorlocalidad,
-  trabajadorprovincia,
-  trabajadorlugardetrabajo,
-  trabajadortareas,
-  trabajadortel,
-  trabajadorcel,
-  socio: req.headers.empleador
-};
+  const data = {
+    trabajadornombre,
+    trabajadorsueldo,
+    trabajadorapellido,
+    trabajadornacionalidad,
+    trabajadorestadocivil,
+    trabajadorsexo,
+    trabajadornacimiento,
+    trabajadordocumento,
+    trabajadorcuil,
+    trabajadordomicilio,
+    trabajadornumdomicilio,
+    trabajadorpiso,
+    trabajadordepto,
+    trabajadorlocalidad,
+    trabajadorprovincia,
+    trabajadorlugardetrabajo,
+    trabajadortareas,
+    trabajadortel,
+    trabajadorcel,
+    socio: req.headers.empleador
+  };
 
-  const empleados = new Empleados(data );
-  
+  const empleados = new Empleados(data);
+
   await empleados.save();
- res.status(201).json({
-    msg:"Empleado creado con exito!",
+  res.status(201).json({
+    msg: "Empleado creado con exito!",
     empleados,
   });
 };
 
-const sociosPut =async(req, res) => {
+const sociosPut = async (req, res) => {
   const { id } = req.params;
-  const {_id,password, email,...restos} = req.body;
-  if(password){
+  const { _id, password, email, ...restos } = req.body;
+  if (password) {
     const salt = bcrypt.genSaltSync();
     restos.password = bcrypt.hashSync(password, salt);
   }
-  
-  const socio = await Socio.findByIdAndUpdate(id,restos,{new:true})
+
+  const socio = await Socio.findByIdAndUpdate(id, restos, { new: true })
   res.status(201).json({
     msg: "Socio Actualizado",
     socio,
   });
 };
-const sociosDelete = async(req, res) => {
+const sociosDelete = async (req, res) => {
   const { id } = req.params;
-  const socioBorrado = await Socio.findByIdAndUpdate(id,{estado:false},{new:true})
+  const socioBorrado = await Socio.findByIdAndUpdate(id, { estado: false }, { new: true })
   res.json({
     msg: "Socio dado de baja  correctamente",
     socioBorrado
   });
- 
+
 };
 
-const sociosDeleteEmpleado = async(req, res) => {
+const sociosDeleteEmpleado = async (req, res) => {
   const { id } = req.params;
-  const socioBorrado = await Socio.empleados.findByIdAndDelete(id);
+  const socioBorrado = await Empleados.findByIdAndDelete(id);
   res.json({
     msg: "Empleado dado de baja  correctamente",
     socioBorrado
   });
- 
+
 };
 
-const obtenerSocio= async (req, res) => {
-  const { id } = req.params;
 
-  const socio = await Socio.findById(id);
-
-  res.json({
-    socio,
-  });
-};
 
 module.exports = {
   sociosGet,
+  socioEmpleado,
   sociosPost,
   empleadosPost,
   sociosPut,
